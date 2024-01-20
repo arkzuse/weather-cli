ffd7fe71fe73cc194f558c8a9ff217f9dde0daba
arkzuse <88483186+arkzuse@users.noreply.github.com>
2024-01-20 20:35:51 +0530

    refactor: location selection logic

--- weather/cli.py
+++ weather/cli.py
@@ -4,24 +4,29 @@ from weather.core import search
 from weather.utils import make_name, get_default_location, set_default_location
 
 
+def handle_no_default():
+    click.echo('Default place not set!')
+
+    new_default = click.prompt('Enter a default place')
+
+    set_default_location(new_default)
+
+    return new_default
+
+
 @click.command()
 @click.argument('place', default=get_default_location(), required=False)
 def main(place):
+    selected = 0
+
     if place == '':
-        click.echo('Default place not set.')
-
-        new_default = click.prompt('Enter a default place')
-
-        set_default_location(new_default)
-        place = new_default
+        place = handle_no_default()
 
     search_result = search(place)
 
     if len(search_result) == 0:
         click.echo('No results found')
         return
-
-    selected = 0
 
     if len(search_result) > 1:
         names = [make_name(obj) for obj in search_result]
